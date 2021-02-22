7dfa5ab49200e95bc0b58171b4b1b3fbaae0349c
Евгений <misha2005_kmv@mail.ru>
2021-02-22 20:50:24 +0300

    Add Task1 - Task6 solution (day 1).

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+        int k = 2;
+        for (int i = 1; i < 10; i++) {
+            int result = i * k;
+            System.out.println(i + " * " + k + " = " + result);
+        }
 
     }
 }

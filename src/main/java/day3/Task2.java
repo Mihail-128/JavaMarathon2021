b22f895a369b0d9973435215e0760ef1633b8506
ulan_ <misha2005_kmv@mail.ru>
2021-02-24 21:10:23 +0300

    Add Task1 - Task3 solution (day 3).

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введите первое число: ");
+            double a = scanner.nextDouble();
+            System.out.print("Введите второе число: ");
+            double b = scanner.nextDouble();
+            if (b == 0) {
+                break;
+            }
+            double result = a / b;
+            System.out.println(result);
+        }
 
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Введите первое число: ");
+            double a = scanner.nextDouble();
+            System.out.print("Введите второе число: ");
+            double b = scanner.nextDouble();
+            if (b == 0) {
+                System.out.println("Деление на ноль");
+                continue;
+            }
+            double result = a / b;
+            System.out.println(result);
 
+        }
     }
 }

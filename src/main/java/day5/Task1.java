c5b84e5c84bd2147486de8aebb89b88e8e3df270
ulan_ <misha2005_kmv@mail.ru>
2021-02-26 18:17:13 +0300

    Add Task1 - Task2 (day 5).

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,42 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setYearOfRelease(1987);
+        car.setColour("Black");
+        car.setModel("Ford");
+        System.out.println(car.getYearOfRelease());
+        System.out.println(car.getColour());
+        System.out.println(car.getModel());
+    }
+}
 
+class Car {
+    private int yearOfRelease;
+    private String colour;
+    private String model;
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,34 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(2014, "Red", "Yamaha");
+        System.out.println(motorbike.getYearOfRelease());
+        System.out.println(motorbike.getColour());
+        System.out.println(motorbike.getModel());
+    }
+}
+
+class Motorbike {
+    private int yearOfRelease;
+    private String colour;
+    private String model;
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Motorbike(int yearOfRelease, String colour, String model) {
+        this.yearOfRelease = yearOfRelease;
+        this.colour = colour;
+        this.model = model;
 
     }
 }
